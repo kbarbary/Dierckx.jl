769cdeb841189f603b73379453517044366418b8
Mus M <mus-m@outlook.com>
2017-04-28 22:12:39 -0400

    explicitly specify 7z

--- deps/build.jl
+++ deps/build.jl
@@ -22,5 +22,5 @@ else # Windows
     catch
         run(`powershell -Command "(new-object net.webclient).DownloadFile(\"$url\", \"ddierckx.7z\")"`)
     end
-    run(`7z x -y ddierckx.7z`)
+    run(`$(joinpath(JULIA_HOME, "7z.exe")) x -y ddierckx.7z`)
 end
